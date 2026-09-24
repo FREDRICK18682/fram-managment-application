09fb5f34945ba762d556743af6364b61a1e7a3eb
fredrick ongoma ochieng <fredrickongima@gmail.com>
2026-09-25 01:06:14 +0300

    this are new files i have created which have tailwing and HTML% froms and templates

--- frammaster/frammaster/urls.py
+++ frammaster/frammaster/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Framer.views import Landing_page
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
    path('', Landing_page, name='landing_page'),
+   path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
